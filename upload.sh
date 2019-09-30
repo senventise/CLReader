0370f50924a6188889f4f45339ffb5fb1c3a837e
Senventise <2375741859@qq.com>
2019-09-30 20:56:36 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
-wget  https://github.com/timvisee/ffsend/releases/download/v0.2.52/ffsend-v0.2.52-linux-x64-static
+wget  -q https://github.com/timvisee/ffsend/releases/download/v0.2.52/ffsend-v0.2.52-linux-x64-static
 mv ffsend-v0.2.52-linux-x64-static ffsend
 chmod +x ffsend
-./ffsend -q upload app/build/outputs/apk/debug/app-debug.apk >> log.txt
+./ffsend -q upload app/build/outputs/apk/debug/app-debug.apk > log.txt
 curl $SERVER_CHAN_URL`cat log.txt`
+echo "下载链接:"`cat log.txt`
